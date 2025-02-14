3b27f6bd492e6c71bcfd2042dae4d7561295c49e
alejandromartinez2104 <digitalcrativeprojects@gmail.com>
2025-02-14 00:29:27 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "amberly-valentine-v1"
+const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/manifest.json", "/placeholder.svg"]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)))
+})
+
